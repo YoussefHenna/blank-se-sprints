b111d3cae1e36f435aac27a99fbfe5d79a0d2dd6
mohamedmostafadawood <mohamed.dawood@student.giu-uni.de>
2021-06-23 00:03:32 +0200

    x

--- client/src/pages/StudentViewGrades/StudentViewGradesPage.tsx
+++ client/src/pages/StudentViewGrades/StudentViewGradesPage.tsx
@@ -1,16 +1,29 @@
 import * as api from "./StudentViewGradesRequests";
 import { useHistory } from "react-router-dom";
 import { useStyles } from "./StudentViewGradesStyles";
+import { getStudentsGrades } from "./StudentViewGradesRequests";
+import { useState } from "react";
+import { useEffect } from "react";
+
 
 const StudentViewGradesPage: React.FC = () => {
   const history = useHistory();
   //Use this to navigate to different screens
   //Example: history.replace("/student/schedule") to navigate to student schedule page
+//Use like this <div className={classes.whateverStyle}/>
+  
+  
+const [grades, setGrades] = useState({})
 
-  //Use like this <div className={classes.whateverStyle}/>
+  useEffect(() => {
+    setGrades(getStudentsGrades())
+  }, []);
+  
+  
+  
   const classes = useStyles();
 
-  return <div>{/** Put ui elements here */}</div>;
+  return <div>{JSON.stringify(grades)}</div>;
 };
 
 export default StudentViewGradesPage;
