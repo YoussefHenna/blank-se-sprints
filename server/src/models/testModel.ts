d82e633eb738b5819af5cc3fb67a3b6aa81343a5
vitaminsofsalem <jokydokyduck@gmail.com>
2021-06-24 04:20:17 +0200

    Removed Test files from MiddleWare

--- server/src/models/testModel.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const testSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-});
-
-const Test = mongoose.model("test", testSchema);
-
-export default Test;
