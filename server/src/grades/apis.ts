c0a316602402c45edd3a7768fe22dd82b0df93a3
Youssef Henna <youssef.hisham14@gmail.com>
2021-06-29 13:06:25 +0200

    added middlewar usage for grades api

--- server/src/grades/apis.ts
+++ server/src/grades/apis.ts
@@ -1,16 +1,40 @@
-import { Router } from "express";
+import { Router, Response } from "express";
 import DatabaseClient from "../database";
 // import * as Operations from "./dbOperations";
 // import * as Exceptions from "./exceptions";
 import { ObjectID, ObjectId } from "mongodb";
 import Student from "../models/studentModel";
 import { CourseGrade } from "../../../SharedObjects/course";
+import Sauth from "../middleware/Sauth";
+import Tauth from "../middleware/Tauth";
+
+const checkIsStudent = (res: Response): string | undefined => {
+  const studentId = res.locals["id"];
+  if (!studentId) {
+    res.send({ error: "Unauthroized request" });
+    return;
+  }
+  return studentId;
+};
+
+const checkIsInstructor = (res: Response): string | undefined => {
+  const tId = res.locals["id"];
+  if (!tId) {
+    res.send({ error: "Unauthroized request" });
+    return;
+  }
+  return tId;
+};
 
 const gradesAPIs = (router: Router, cl: DatabaseClient) => {
   //For now use this student ID, change to use value in cookie later (after auth finished)
-  const studentId = "60cc811b111a71a2f67da382";
 
+  router.use(["/student/grades"], Sauth);
   router.get("/student/grades", async (req, res) => {
+    const studentId = checkIsStudent(res);
+    if (!studentId) {
+      return;
+    }
     try {
       //First find the student with the given ID
       //Check models/studentModel.ts - I updated it to include array of grades
@@ -35,22 +59,33 @@ const gradesAPIs = (router: Router, cl: DatabaseClient) => {
     }
   });
 
-  router.patch("/student/grades", async (req, res) => {
+  router.use(["/instructor/grades"], Tauth);
+  router.patch("/instructor/grades", async (req, res) => {
+    const tId = checkIsInstructor(res);
+    if (!tId) {
+      return;
+    }
     try {
       //Request should include these in the body, to know which course grade to change
       //and to which student
-      const { courseId, studentId, grade } = req.body;
+      const { courseId, studentUsername, grade } = req.body;
 
       //First find the student and check if a grade already exists for the given course
       //If so then remove it
-      await Student.findByIdAndUpdate(studentId, {
-        $pull: { grades: { courseId: new ObjectID(courseId) } },
-      });
+      await Student.findOneAndUpdate(
+        { username: studentUsername },
+        {
+          $pull: { grades: { courseId: new ObjectID(courseId) } },
+        }
+      );
 
       //Add the new/updated grade to the list
-      await Student.findByIdAndUpdate(studentId, {
-        $push: { grades: { courseId: new ObjectID(courseId), grade } },
-      });
+      await Student.findOneAndUpdate(
+        { username: studentUsername },
+        {
+          $push: { grades: { courseId: new ObjectID(courseId), grade } },
+        }
+      );
 
       //Send empty response, indicating operation has succeeded
       res.send();
@@ -59,8 +94,36 @@ const gradesAPIs = (router: Router, cl: DatabaseClient) => {
       res.status(500).send({ error: "Server error" });
     }
   });
+
+  router.get("/instructor/grades/:studentUsername", async (req, res) => {
+    const tId = checkIsInstructor(res);
+    if (!tId) {
+      return;
+    }
+    try {
+      //First find the student with the given ID
+      //Check models/studentModel.ts - I updated it to include array of grades
+      const data = await Student.findOne({
+        username: req.params.studentUsername,
+      });
+
+      //If has errors or data isnt retrieved, the send an error in response
+      if (data.errors || data === undefined) {
+        //400 is used to indicate error
+        //Send json object with error details, to be able to know what went wrong
+        res.status(400).send({ error: "No student with given username" });
+      } else {
+        //Send the grades in a new object with key grades, if it exists
+        if (data.get("grades")) {
+          res.send({ grades: data.get("grades") });
+        } else {
+          //send empty list if student doesnt have any grades
+          res.send({ grades: [] });
+        }
+      }
+    } catch (e) {
+      res.status(500).send({ error: "Server error" });
+    }
+  });
 };
 export default gradesAPIs;
-
-
-
